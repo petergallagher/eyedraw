d58b2005db4fb2fa110f141d026e046328cece9c
Jamie Neil <jamie.neil@openeyes.org.uk>
2012-11-15 10:12:51 +0000

    Add foster images to form Cross format

--- views/OEEyeDrawWidgetGonioscopy.php
+++ views/OEEyeDrawWidgetGonioscopy.php
@@ -48,44 +48,61 @@
 	<canvas id="<?php echo $canvasId?>" class="<?php if ($isEditable) { echo 'edit'; } else { echo 'display'; }?>" width="<?php echo $size?>" height="<?php echo $size?>" tabindex="1"<?php if ($canvasStyle) {?> style="<?php echo $canvasStyle?>"<?php }?>></canvas>
 	<?php if ($isEditable) {?>
 	<div class="eyedrawFields">
-		<div<?php if(!$model->getSetting('expert')) { ?> style="display: none;"<?php } ?>>
-			<div class="data">
-				<?php echo CHtml::activeDropDownList($model, $side.'_gonio_sup_id', $model->getGonioscopyOptions(), array('class' => 'gonioGrade gonioExpert', 'data-position' => 'sup'))?>
-			</div>
-			<div class="data">
-				<?php echo CHtml::activeDropDownList($model, $side.'_gonio_tem_id', $model->getGonioscopyOptions(), array('class' => 'gonioGrade gonioExpert', 'data-position' => 'tem'))?>
-			</div>
-			<div class="data">
-				<?php echo CHtml::activeDropDownList($model, $side.'_gonio_nas_id', $model->getGonioscopyOptions(), array('class' => 'gonioGrade gonioExpert', 'data-position' => 'nas'))?>
-			</div>
-			<div class="data">
-				<?php echo CHtml::activeDropDownList($model, $side.'_gonio_inf_id', $model->getGonioscopyOptions(), array('class' => 'gonioGrade gonioExpert', 'data-position' => 'inf'))?>
+		<div <?php if(!$model->getSetting('expert')) { ?>
+			style="display: none;" <?php } ?>>
+			<div class="label">Scheie grade:</div>
+			<div class="data gonioCross">
+				<div class="gonioSup">
+					<?php echo CHtml::activeDropDownList($model, $side.'_gonio_sup_id', $model->getGonioscopyOptions(), array('class' => 'gonioGrade gonioExpert', 'data-position' => 'sup'))?>
+				</div>
+				<div class="gonioTem">
+					<?php echo CHtml::activeDropDownList($model, $side.'_gonio_tem_id', $model->getGonioscopyOptions(), array('class' => 'gonioGrade gonioExpert', 'data-position' => 'tem'))?>
+				</div>
+				<div class="gonioNas">
+					<?php echo CHtml::activeDropDownList($model, $side.'_gonio_nas_id', $model->getGonioscopyOptions(), array('class' => 'gonioGrade gonioExpert', 'data-position' => 'nas'))?>
+				</div>
+				<div class="gonioInf">
+					<?php echo CHtml::activeDropDownList($model, $side.'_gonio_inf_id', $model->getGonioscopyOptions(), array('class' => 'gonioGrade gonioExpert', 'data-position' => 'inf'))?>
+				</div>
 			</div>
 		</div>
 		<?php if($model->getSetting('expert')) { ?>
 		<div>
 			<div class="label">
-				<?php echo $model->getAttributeLabel($side.'_van_herick_id'); ?>
-				:
+				<?php echo $model->getAttributeLabel($side.'_van_herick_id'); ?> (<?php echo CHtml::link('images', '#', array('class' => 'foster_images_link')); ?>):
 			</div>
 			<div class="data">
-				<?php echo CHtml::activeDropDownList($model, $side.'_van_herick_id', $model->getVanHerickOptions())?>
+				<?php echo CHtml::activeDropDownList($model, $side.'_van_herick_id', $model->getVanHerickOptions()); ?>
+				<div data-side="<?php echo $side?>" class="foster_images_dialog" title="Foster Images">
+					<img usemap="#<?php echo $side ?>_foster_images_map" src="<?php echo $imgPath ?>gonioscopy.png">
+					<map name="<?php echo $side ?>_foster_images_map">
+						<area data-vh="5" shape="rect" coords="0,0,225,225" />
+						<area data-vh="15" shape="rect" coords="0,225,225,450" />
+						<area data-vh="25" shape="rect" coords="0,450,225,675" />
+						<area data-vh="30" shape="rect" coords="225,0,450,225" />
+						<area data-vh="75" shape="rect" coords="225,225,450,450" />
+						<area data-vh="100" shape="rect" coords="225,450,450,675" />
+					</map>
+				</div>
 			</div>
 		</div>
 		<?php } else { ?>
 		<div>
+			<div class="label">Pigmented meshwork seen:</div>
 			<?php $basic_options = array('0' => 'No', '1' => 'Yes'); ?>
-			<div class="data">
-				<?php echo CHtml::dropDownList($side.'_gonio_sup_basic', null, $basic_options, array('class' => 'gonioGrade gonioBasic', 'data-position' => 'sup'))?>
-			</div>
-			<div class="data">
-				<?php echo CHtml::dropDownList($side.'_gonio_tem_basic', null, $basic_options, array('class' => 'gonioGrade gonioBasic', 'data-position' => 'tem'))?>
-			</div>
-			<div class="data">
-				<?php echo CHtml::dropDownList($side.'_gonio_nas_basic', null, $basic_options, array('class' => 'gonioGrade gonioBasic', 'data-position' => 'nas'))?>
-			</div>
-			<div class="data">
-				<?php echo CHtml::dropDownList($side.'_gonio_inf_basic', null, $basic_options, array('class' => 'gonioGrade gonioBasic', 'data-position' => 'inf'))?>
+			<div class="data gonioCross">
+				<div class="gonioSup">
+					<?php echo CHtml::dropDownList($side.'_gonio_sup_basic', null, $basic_options, array('class' => 'gonioGrade gonioBasic', 'data-position' => 'sup'))?>
+				</div>
+				<div class="gonioTem">
+					<?php echo CHtml::dropDownList($side.'_gonio_tem_basic', null, $basic_options, array('class' => 'gonioGrade gonioBasic', 'data-position' => 'tem'))?>
+				</div>
+				<div class="gonioNas">
+					<?php echo CHtml::dropDownList($side.'_gonio_nas_basic', null, $basic_options, array('class' => 'gonioGrade gonioBasic', 'data-position' => 'nas'))?>
+				</div>
+				<div class="gonioInf">
+					<?php echo CHtml::dropDownList($side.'_gonio_inf_basic', null, $basic_options, array('class' => 'gonioGrade gonioBasic', 'data-position' => 'inf'))?>
+				</div>
 			</div>
 		</div>
 		<?php } ?>
@@ -98,7 +115,6 @@
 				<?php echo CHtml::activeTextArea($model, $side.'_description', array('rows' => "2", 'cols' => "20", 'class' => 'autosize')) ?>
 			</div>
 		</div>
-		<div>TODO: Foster images</div>
 		<button class="ed_report">Report</button>
 		<button class="ed_clear">Clear</button>
 	</div>
@@ -111,18 +127,20 @@
 		</div>
 		<div>
 			<div class="data">
-				<?php if($gonio = $model->{$side.'_gonio_sup'}) { 
-					echo $gonio->name;
-				} ?>
-				<?php if($gonio = $model->{$side.'_gonio_tem'}) { 
-					echo $gonio->name;
-				} ?>
-				<?php if($gonio = $model->{$side.'_gonio_nas'}) { 
-					echo $gonio->name;
-				} ?>
-				<?php if($gonio = $model->{$side.'_gonio_inf'}) { 
-					echo $gonio->name;
-				} ?>
+				<div class="data gonioCross">
+					<div class="gonioSup">
+						<?php echo $model->{$side.'_gonio_sup'}->name; ?>
+					</div>
+					<div class="gonioTem">
+						<?php echo $model->{$side.'_gonio_tem'}->name; ?>
+					</div>
+					<div class="gonioNas">
+						<?php echo $model->{$side.'_gonio_nas'}->name; ?>
+					</div>
+					<div class="gonioInf">
+						<?php echo $model->{$side.'_gonio_inf'}->name; ?>
+					</div>
+				</div>
 			</div>
 		</div>
 		<?php if($model->{$side.'_van_herick_id'} || $model->getSetting('expert')) { ?>
